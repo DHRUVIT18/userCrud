5abefd5c16472ee71bde2a08c624f7ea16693a7d
dhruvit18 <dhruvitpatel2811@gmail.com>
2023-09-26 12:04:29 +0530

    crud-backend

---
+++ app.js
@@ -0,0 +1,23 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+require("./db")
+const authRoutes = require('./routes/authRoutes');
+// const taskRouter = require('./routes/taskRoutes');
+const morgan = require('morgan');
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/auth', authRoutes);
+
+
+app.get("/",(req,res)=>{
+    res.send("backend running");
+})
+
+const port = 4000;
+
+app.listen(port, () => {
+	console.log(`server is running on port`, port);
+});
